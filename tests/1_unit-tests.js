fdb5fd506d33867c8e6e881ee4e00e3e43351353
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-03-23 18:55:49 +0000

    matches the second argument regular expression.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -125,11 +125,12 @@ test('Array #include, #notInclude', function() {
       assert.include('Arrow', 'row', "Arrow contains row...");
       assert.notInclude('dart', 'queue', "But a dart doesn't contain a queue");
     });
-    // #15
-    test('#match, #notMatch', function () {
-      const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-      assert.fail(formatPeople('John Doe', 35), regex);
-      assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
+    /** 15 - #match Asserts that the actual value **/
+    // matches the second argument regular expression.
+    test('#match, #notMatch', function() {
+      var regex =  /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
+      assert.match(formatPeople('John Doe', 35), regex);
+      assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });
   });
 
