4879fdba4bc5591b7f50314fb2d20cc1efd64cfe
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-03-28 10:44:35 +0100

    Functional Tests 1 and 2 on API Endpoints using Chai-HTTP

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,26 +8,35 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
   suite("Integration tests with chai-http", function () {
-    // #1
-    test("Test GET /hello with no name", function (done) {
-      chai
-        .request(server)
-        .get("/hello")
-        .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
-          done();
-        });
+    /** Ready to have a try ?
+	* Replace assert.fail(). Make the test pass. **/
+ 
+// If no name is passed, the endpoint responds with 'hello Guest'.
+test('Test GET /hello with no name',  function(done){ // Don't forget the callback...
+   chai.request(server)             // 'server' is the Express App
+    .get('/hello')                  // http_method(url). NO NAME in the query !
+    .end(function(err, res){        // res is the response object
+    
+      // Test the status and the text response (see the example above). 
+      // Please follow the order -status, -text. We rely on that in our tests.
+      // It should respond 'Hello Guest'
+      assert.equal(res.status, 200);
+      assert.equal(res.text, 'hello Guest');
+      done();   // Always call the 'done()' callback when finished.
     });
+});
     // #2
     test("Test GET /hello with your name", function (done) {
       chai
         .request(server)
         .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
-          done();
+      // Your tests here.
+      // Replace assert.fail(). Make the test pass.
+      // Test the status and the text response. Follow the test order like above.
+      assert.equal(res.status, 200);
+       assert.equal(res.text, 'hello xy_z'/** <==  Put your name here **/);
+      done();   // Always call the 'done()' callback when finished.
         });
     });
     // #3
